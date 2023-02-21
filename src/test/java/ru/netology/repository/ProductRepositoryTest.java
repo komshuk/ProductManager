dccd79a03aee59a08b99076dec54f9e4ca41f498
Neto Max <komshuk@gmail.com>
2023-02-21 22:24:41 +0300

    Initian commit

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,22 +1,43 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.exception.NotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProductRepositoryTest {
-    private ProductRepository repository = new ProductRepository();
-    private Book firstBook = new Book(1, "Alice’s Adventures in Wonderland", 228, "Lewis Carroll");
-    private Book secondBook = new Book(2, "Through the Looking-Glass, and What Alice Found There", 307, "Lewis Carroll");
+public class ProductRepositoryTest {
+    private final ProductRepository repository = new ProductRepository();
+    private final Book firstBook = new Book(1, "Alice’s Adventures in Wonderland", 228, "Lewis Carroll");
+    private final Book secondBook = new Book(2, "Through the Looking-Glass, and What Alice Found There", 307, "Lewis Carroll");
+    private final Smartphone firstSmartphone = new Smartphone(3, "8.3", 49_990, "Nokia");
+    private final Smartphone secondSmartphone = new Smartphone(4, "5.3 4/64", 15_990, "Nokia");
 
-    @Test
-    public void shouldRemoveByID() {
+
+    @BeforeEach
+    public void setUp() {
         repository.save(firstBook);
         repository.save(secondBook);
-        Product[] expected = new Product[]{secondBook};
-        repository.removeById(1);
-        Product[] actual = repository.findAll();
+        repository.save(firstSmartphone);
+        repository.save(secondSmartphone);
+    }
+
+    @Test
+    public void mustDeleteAnExistingElement() {
+        Product[] expected = new Product[]{firstBook, secondBook, firstSmartphone};
+        Product[] actual = repository.removeById(4);
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowAnExceptionWhenTryingToDeleteNonExistentElement() {
+        int id = 5;
+
+        assertThrows(NotFoundException.class, () -> repository.removeById(5));
+        assertThrows(NotFoundException.class, () -> repository.removeById(5), "Element with id: " + id + " not found");
     }
 }
